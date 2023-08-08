d5bae8b14818c7da422710197e256c126b5257ff
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-08 19:29:02 -0300

    first commit

---
+++ Fiap.HelloWorld/Fiap.HelloWorld.UI/Models/Aluno.cs
@@ -0,0 +1,23 @@
+﻿using Fiap.HelloWorld.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Fiap.HelloWorld.UI
+{
+    public class Aluno : Pessoa
+    {
+        //Propriedades
+        public DateTime DataMatricula { get; set; }
+        public bool Ativo { get; set; }
+
+    }
+}
+
+
+
+
+    
---
+++ Fiap.HelloWorld/Fiap.HelloWorld.UI/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Fiap.HelloWorld.UI;
+using Fiap.HelloWorld.UI.Models;
+
+Console.WriteLine("Hello, World!");
+
+//Instanciar um Aluno
+Aluno aluno = new Aluno();
+
+//Atribuir o nome ao aluno
+aluno.Nome = "João";
+aluno.Cpf = "1234234";
+
+//Exibir o nome
+Console.WriteLine(aluno.Nome);
+Console.WriteLine(aluno.Cpf);
+
+//Instanciar um Cachorro
+Cachorro dog = new Cachorro(15, 5, "Brutus")
+{
+    Dono = "Thiago"
+};
+
+//Exibir o nome, peso, idade e dono
+Console.WriteLine($"Nome: {dog.Nome}, Peso: {dog.Peso}, Idade: {dog.Idade}, Dono: {dog.Dono}");
